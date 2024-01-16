2429bbfb7a4d90b6a0d3a46d8b205e2a66a2ef69
alejo <alejoville04@gmail.com>
2024-01-16 11:42:31 -0300

    git commit --amend

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,9 +1,6 @@
- // middleware/errorHandler.js
- const errorHandler = (err, req, res, next) => {
-    // Verificar si el error tiene un código de estado definido, de lo contrario, establecer el código de estado predeterminado
+const errorHandler = (err, req, res, next) => {
     const statusCode = err.statusCode || 500;
   
-    // Construir objeto de respuesta de error
     const errorResponse = {
       error: {
         message: err.message || "Error interno del servidor",
@@ -11,7 +8,6 @@
       },
     };
   
-    // Enviar respuesta de error en formato JSON
     res.status(statusCode).json(errorResponse);
   };
   
